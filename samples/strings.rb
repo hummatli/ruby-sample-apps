f9b2bd9ba2b9a12c97b58242d263a38c69dddd0e
Sattar Hummatli <settarxan@gmail.com>
2017-12-24 09:17:33 +0400

    add string samples

---
+++ samples/strings.rb
@@ -0,0 +1,29 @@
+x  = 'hello'
+
+y  = "world"
+
+ greeting = x + " " +  y
+
+ puts greeting
+
+z = ''' sdsd 
+    sds \t
+'''
+puts z
+
+k = """ sdsd 
+    sds \t
+"""
+puts k
+
+puts "\ta\tb\nc\nd"
+puts '\ta\tb\nc\nd'
+
+puts "I want to say #{greeting}"
+
+puts 'I want to say #{greeting}'
+
+d = 1
+puts "My val is #{d}"
+
+puts greeting[1]
