75c7589b9b12848b4e03c716b4a43395ecd723d1
Sattar Hummatli <settarxan@gmail.com>
2017-12-24 10:35:52 +0400

    add symbol type sample

---
+++ samples/symbols.rb
@@ -0,0 +1,14 @@
+#it will return different ids
+puts "string".object_id
+puts "string".object_id
+
+#it will return same ids
+puts :simbol.object_id
+puts :simbol.object_id
+
+puts :simbol.class
+
+
+hash = {:first_name => 'Sattar', :last_name => "Hummatli"}
+
+puts hash[:first_name]
