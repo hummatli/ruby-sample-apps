6fffc6c2723c79921a1a3aa3507556f0e03afc2c
Sattar Hummatli <settarxan@gmail.com>
2017-12-24 12:23:33 +0400

    add conditional short opertaors

--- samples/conditionals.rb
+++ samples/conditionals.rb
@@ -11,7 +11,13 @@ end
 puts st
 
 
-x, y = 1000, 100
+unless x < y
+   st = 'x is less than y'
+else 
+   st = 'x is greater than y'
+end
+puts st
+
 
 if x < y; st = 'x is less than y'
 elsif x == y; st = 'x is equal y'
@@ -20,6 +26,26 @@ end
 puts st
 
 
-#terenary conditional
+#inine if
 s = "bob"
 puts "This is Bob" if s == 'bob'
+
+#terenary opertaor
+
+b = 1 == 1 ? "equal" : "not equal"
+puts b
+
+#or /or-equals
+b = nil
+if b 
+    puts "is not nill"
+else
+    puts 'is nil'
+end
+
+x = b || 'default val'
+
+puts x
+
+z ||= "s"
+puts z
