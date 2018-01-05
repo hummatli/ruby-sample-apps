4693383b48bf7a26b5d7060e2aced44b5c78f815
Sattar Hummatli <settarxan@gmail.com>
2018-01-05 16:44:52 +0400

    add instance attributes, class attributes and methods

--- samples/classes.rb
+++ samples/classes.rb
@@ -3,9 +3,32 @@ class Animal
     attr_writer :color
     attr_reader :legs, :arms
 
-    def initialize
+    @@species = ['cat', 'cow', 'dog', 'duck', 'horse']
+    @@current_animals = []
+
+    def self.species
+        @@species
+    end
+
+    def self.species=(array=[])
+        @@species = array
+    end
+
+    def self.current_animals
+        @@current_animals
+    end
+
+    def self.create_with_attributes(noise, color)
+        animal = self.new(noise)
+        animal.color = color
+        return animal
+    end
+
+    def initialize(noise)
+        @noise = noise
         @legs = 4
         @arms = 2 
+        @@current_animals << self
     end
 
     def noise=(noise)
@@ -17,7 +40,14 @@ class Animal
     end
 end
 
-animal = Animal.new
-animal.noise = "Moo"
+puts Animal.species.inspect
+
+animal = Animal.new("Moo")
 animal.name = "Inek"
 puts animal.noise + " " + animal.name + " " + animal.legs.to_s + " " + animal.arms.to_s
+
+animal2 = Animal.create_with_attributes("Quack", "red")
+animal2.name = "duck"
+puts animal2.noise + " " + animal2.name + " " + animal.legs.to_s + " " + animal.arms.to_s
+
+puts Animal.current_animals
