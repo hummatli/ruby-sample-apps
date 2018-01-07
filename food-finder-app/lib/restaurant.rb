b022ba50b87f0b24f9d5f729044c50845974834d
Sattar Hummatli <settarxan@gmail.com>
2018-01-07 13:16:56 +0400

    Refactor add functionality

--- food-finder-app/lib/restaurant.rb
+++ food-finder-app/lib/restaurant.rb
@@ -35,6 +35,26 @@ class Restaurant
     # return instances of restaurant
   end
 
+  def self.build_using_questions
+    args = {}
+    print "Restaurant name: "
+    args[:name] = gets.chomp.strip
+
+    print "Cuisine type: "
+    args[:cuisine] = gets.chomp.strip
+    
+    print "Average price: "
+    args[:price] = gets.chomp.strip
+
+    return self.new(args)
+  end
+
+  def initialize(args={})
+    @name     = args[:name]     || ""
+    @cuisine  = args[:cuisine]  || ""
+    @price    = args[:price]    || ""
+  end
+
   def save
     return false unless Restaurant.file_usable?
     File.open(@@filepath, 'a') do |file|
